4dc0a9e016746c375dba41f4c7ff9f92560f52ac
dohyeons <ddhhss0603@gmail.com>
2022-08-14 15:32:52 +0900

    시계 완료

--- js/greetings.js
+++ js/greetings.js
@@ -5,9 +5,11 @@ const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME ='hidden'
 const USERNAME_KEY = 'username'
 const savedUsername = localStorage.getItem(USERNAME_KEY);
+
 function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value
     localStorage.setItem(USERNAME_KEY, username)
     paintGreeting()
 
