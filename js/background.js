4af07b4d06315ba7b46b7c4e5b37f55752946d00
dohyeons <ddhhss0603@gmail.com>
2022-08-24 21:56:35 +0900

    배경화면 작업

--- js/background.js
+++ js/background.js
@@ -2,9 +2,6 @@ const images = ["0.jpeg", "1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg", "5.jpeg","6.jp
 
 const chosenImage = images[Math.floor(Math.random()*images.length)]
 
-const image = document.createElement('img');
-image.src = `img/${chosenImage}`;
-
 const body = document.querySelector('body')
-// body.style.backgroundImage = `url(img/${chosenImage})`
-body.appendChild(image);
+body.style.backgroundImage = `url(img/${chosenImage})`
+console.dir(body)
